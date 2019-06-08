696a8a7c721bb68f35117e6a932e36f70aada684
AndreyPashkin <druidan@mail.ru>
2019-06-08 22:56:06 +0300

    fonts added

--- central_server_ui/js/script.js
+++ central_server_ui/js/script.js
@@ -9,7 +9,30 @@ let app = new Vue({
         ],
         profile: {
             avatar: '../html/img/photo_s.jpg',
-            fio: 'Фамилия Имя Отчество'
+            photo: '../html/img/photo.png',
+            name1: 'Василиса',
+            name2: 'Белова',
+            name3: 'Андреевна',
+            schoolName: '',
+            schoolID: '',
+            inn: '123123123123',
+            snils: '123-123-123-12',
+            passport:{
+                passportNumber: '1221 980890',
+                gettingDate: '09.09.1999',
+                unitCode: '123-123',
+                whoGave: 'Отделом УФМС №123 г. Пермь',
+                registrationAddress: '',
+                birthday: '01.01.1980',
+                birthPlace: '',
+            },
+            contacts: {
+                phone: '(123)123-12-12',
+                email: 'test@test.test',
+            },
+            livingPlace: '',
+            parentsData: '',
+
         }
     },
     methods: {
