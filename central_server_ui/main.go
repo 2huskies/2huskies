c62d3bde02fd1f0f13a77c2d4fcceaae7b39b420
i1 <i1@17bit.online>
2019-06-08 17:38:05 +0300

    binary

--- central_server_ui/main.go
+++ central_server_ui/main.go
@@ -6,9 +6,7 @@ import (
 	"log"
 	"mime"
 	"net/http"
-	"os"
 	"path"
-	"path/filepath"
 	"strings"
 )
 
@@ -16,8 +14,6 @@ var (
 	bind    string
 	logFile string
 )
-
-var host *Host
 
 type App struct {
 	IndexHandler *IndexHandler
@@ -43,37 +39,17 @@ func (a *App) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	flag.StringVar(&bind, "b", ":3080", "`адрес:порт` на котором слушать запросы")
-	flag.StringVar(&logFile, "l", "", "`файл` журнала")
 	flag.Parse()
 
 	var err error
-	if logFile == "" {
-		logger = log.New(os.Stdout, "", log.LstdFlags)
-	} else {
-		output, err := filepath.Abs(logFile)
-		if err != nil {
-			log.Fatalf("%s\n", err)
-		}
-		fo, err := os.OpenFile(output, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("open %s: %s\n", output, err)
-		}
-		// close fo on exit and check for its returned error
-		defer func() {
-			if err := fo.Close(); err != nil {
-				log.Fatalf("close %s\n", err)
-			}
-		}()
-		logger = log.New(fo, "", log.LstdFlags)
-	}
 	a := &App{
 		IndexHandler: new(IndexHandler),
 	}
-	logger.Printf("listening on: %s", bind)
+	log.Printf("listening on: %s", bind)
 	mime.AddExtensionType(".js", "text/javascript")
 	err = http.ListenAndServe(bind, a)
 	if err != nil {
-		logger.Fatalf("Listen error: %s\n", err)
+		log.Fatalf("Listen error: %s\n", err)
 	}
 }
 
