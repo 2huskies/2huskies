b924b9ffdde8daf56bbbe11dd4ad056d870c0baf
Artyom Tetiukhin <artemtetiukhin@yandex.com>
2019-06-08 15:47:21 +0300

    Add schema database

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS abiturient (
+    id           SERIAL PRIMARY KEY,
+    first_name   TEXT   NOT NULL,
+    last_last    TEXT   NOT NULL,
+    birth_date   DATE   NOT NULL,
+    birth_place  TEXT   NOT NULL,  
+    address      TEXT   NOT NULL,   
+    phone_number TEXT   NOT NULL     
+);
+
+CREATE TABLE IF NOT EXISTS subject (
+    id   SERIAL PRIMARY KEY,
+    name TEXT   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS score (
+    abiturient_id INTEGER NOT NULL,
+    subject_id    INTEGER NOT NULL,
+    score         INTEGER CHECK (score >= 0 AND score <= 100),
+	
+    PRIMARY KEY (abiturient_id, subject_id),
+    CONSTRAINT abiturient_fkey FOREIGN KEY (abiturient_id) REFERENCES  abiturient(id) MATCH SIMPLE ON DELETE NO ACTION,
+    CONSTRAINT subject_fkey    FOREIGN KEY (subject_id)    REFERENCES  subject(id)    MATCH SIMPLE ON DELETE NO ACTION
+);
